cd9c8577e316b9b534c9d2385f50720fe057a468
Kulu <randazzo.lucas@gmail.com>
2017-08-09 22:42:17 +0200

    Adding swag

--- Jukebox.py
+++ Jukebox.py
@@ -162,7 +162,7 @@ class LinkHandler:
         if self.duration == "none":
             self.duration = None
         else:
-            self.duration = int_to_time(int(self.suration))
+            self.duration = int_to_time(int(self.duration))
         try:
             os.remove(os.path.expanduser(infofind))
         except:
